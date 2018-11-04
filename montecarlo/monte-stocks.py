9bf4a92cee0973cdf08f179608c5aba643053879
iyamoto@gmail.com <5tgb8ik,5tgb>
2018-11-04 13:17:38 +0300

    spiking

--- montecarlo/monte-stocks.py
+++ montecarlo/monte-stocks.py
@@ -7,63 +7,97 @@ https://www.moex.com/ru/derivatives/contracts.aspx
 import time
 import sys
 import os
+import json
 sys.path.insert(0, os.path.abspath('..'))
 import libs.futures
+import configs.alphaconf
+from pprint import pprint
 
 min_goal = 0.1
-min_RewardRiskRatio = 2
+min_RewardRiskRatio = 5
 atr_multiplier = 5
 
-symbol = 'MOEX'
+results = dict()
+datatype = 'm'
 
-print(symbol)
+if datatype == 'm':
+    watchdata = configs.alphaconf.symbols_m
+else:
+    watchdata = configs.alphaconf.symbols
 
-futures = libs.futures.FUTURES(symbol=symbol, boardid='TQBR')
-futures.get_data_from_moex(cachedir=os.path.join('..', 'cache-m'))
+for item in watchdata:
 
-futures.get_atr(period=5)
-futures.get_ema(period=5)
-futures.get_kc()
+    # Parse watchlist
+    if type(item) == dict:
+        price = list(item.values())[0]
+        symbol = list(item.keys())[0]
+    else:
+        symbol = item
+        price = 0
 
-futures.df.pop('Open')
-futures.df.pop('High')
-futures.df.pop('Low')
+    print(symbol)
 
-# futures.plot()
+    futures = libs.futures.FUTURES(symbol=symbol, boardid='TQBR')
+    futures.get_data_from_moex(cachedir=os.path.join('..', 'cache-m'))
 
-futures.df.pop('KC_LOW')
-futures.df.pop('KC_HIGH')
+    futures.get_atr(period=5)
+    futures.get_ema(period=5)
+    futures.get_kc()
 
-# Stop loss
-futures.df['StopLoss'] = futures.df['Close'] - atr_multiplier * futures.df['ATR']
-futures.df['StopLossPercent'] = 1 - futures.df['StopLoss'] / futures.df['Close']
-bust = futures.df['StopLossPercent'].max()
+    futures.df.pop('Open')
+    futures.df.pop('High')
+    futures.df.pop('Low')
 
-print('Last price:', round(futures.df.Close[-1:].values[0], 2))
-print('Bust:', bust)
-print('StopLoss:', round(futures.df.Close[-1:].values[0]*(1-bust), 2))
-print()
+    # futures.plot()
 
-bust_chance, goal_chance = futures.get_bust_chance(bust=bust, sims=10000, goal=min_goal)
-print('Bust chance:', round(bust_chance, 2))
-print('Goal chance:', round(goal_chance, 2))
-print()
+    futures.df.pop('KC_LOW')
+    futures.df.pop('KC_HIGH')
 
-# Reward-risk ratio
-for i in range(1, 10):
-    goal = i * min_goal
-    futures.df['RewardRiskRatio'] = (goal_chance/i) * goal * futures.df['Close'] / \
-        (bust_chance * (futures.df['Close'] - futures.df['StopLoss']))
-    if futures.df['RewardRiskRatio'].mean() > min_RewardRiskRatio:
-        break
+    # Stop loss
+    futures.df['StopLoss'] = futures.df['Close'] - atr_multiplier * futures.df['ATR']
+    futures.df['StopLossPercent'] = 1 - futures.df['StopLoss'] / futures.df['Close']
+    bust = futures.df['StopLossPercent'].max()
 
-print('Reward-Risk ratio:', futures.df['RewardRiskRatio'].mean())
-print('Goal:', goal)
-print('Exit price:', round(futures.df.Close[-1:].values[0] * (1 + goal), 2))
-print()
+    print('Last price:', round(futures.df.Close[-1:].values[0], 2))
+    print('Bust:', bust)
+    print('StopLoss:', round(futures.df.Close[-1:].values[0]*(1-bust), 2))
+    print()
 
-if goal > min_goal:
-    print('RewardRiskRatio is to low, calculating new goal chances')
-    bust_chance, goal_chance = futures.get_bust_chance(bust=bust, goal=goal)
+    bust_chance, goal_chance = futures.get_bust_chance(bust=bust, sims=10000, goal=min_goal)
+    print('Bust chance:', round(bust_chance, 2))
     print('Goal chance:', round(goal_chance, 2))
     print()
+
+    # Reward-risk ratio
+    for i in range(1, 5):
+        goal = i * min_goal
+        if bust_chance < 0.001:
+            bust_chance = 0.001
+        futures.df['RewardRiskRatio'] = (goal_chance/i) * goal * futures.df['Close'] / \
+            (bust_chance * (futures.df['Close'] - futures.df['StopLoss']))
+        if futures.df['RewardRiskRatio'].mean() > min_RewardRiskRatio:
+            break
+
+    print('Reward-Risk ratio:', futures.df['RewardRiskRatio'].mean())
+    print('Goal:', goal)
+    print('Exit price:', round(futures.df.Close[-1:].values[0] * (1 + goal), 2))
+    print()
+
+    if goal > min_goal:
+        print('RewardRiskRatio is to low, calculating new goal chances')
+        bust_chance, goal_chance = futures.get_bust_chance(bust=bust, goal=goal)
+        print('Goal chance:', round(goal_chance, 2))
+        print()
+    else:
+        results[symbol] = dict()
+        results[symbol]['goal'] = goal
+        results[symbol]['goal_chance'] = round(goal_chance, 2)
+        results[symbol]['bust'] = round(bust, 2)
+        results[symbol]['bust_chance'] = round(bust_chance, 2)
+        results[symbol]['last_price'] = round(futures.df.Close[-1:].values[0], 2)
+        results[symbol]['stop_loss'] = round(futures.df.Close[-1:].values[0]*(1-bust), 2)
+        results[symbol]['exit_price'] = round(futures.df.Close[-1:].values[0] * (1 + goal), 2)
+        results[symbol]['reward_risk_ratio'] = round(futures.df['RewardRiskRatio'].mean(), 2)
+
+print('Results')
+pprint(results)
