8a6dea98130fdbd1a656ae004ff8ff694d7fe2ad
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-09-21 16:32:59 +0300

    spiking

--- tools/check-correlations.py
+++ tools/check-correlations.py
@@ -6,18 +6,44 @@ https://www.moex.com/s221
 import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
-import fire
+import pandas as pd
 import advisor
 
 if __name__ == "__main__":
 
     adv = advisor.ADVISOR(datatype='me', plot_anomaly=False)
-    etfs = [
-        'FXRL',
-        'FXUS',
-        'FXRU',
-        'FXGD'
-    ]
-    rez = adv.correlation(datatype1='me', symbol1='FXGD', datatype2='me', symbol2='FXRU')
-    print(rez)
 
+    # # Portfolio FX-Kub
+    # etfs = [
+    #     'FXRL',
+    #     'FXUS',
+    #     'FXRU',
+    #     'FXGD',
+    #     'FXTB'
+    # ]
+
+    # Portfolio Sber
+    etfs = [
+        'SBMX',
+        'SBSP',
+        'SBGB',
+        'SBCB',
+    ]
+
+    data = dict()
+    data['USD/RUB'] = list()
+    data['Name'] = list()
+    for etf0 in etfs:
+        data['Name'].append(etf0)
+        data[etf0] = list()
+
+        for etf1 in etfs:
+            rez = round(adv.correlation(datatype1='me', symbol1=etf0, datatype2='me', symbol2=etf1), 1)
+            data[etf0].append(rez)
+
+        # USD/RUB correlation
+        rez = round(adv.correlation(datatype1='me', symbol1=etf0, datatype2='mc', symbol2='USD000UTSTOM'), 1)
+        data['USD/RUB'].append(rez)
+
+    df = pd.DataFrame(data)
+    print(df)
