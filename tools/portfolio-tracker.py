c8c8fb90b8a396e2acf332524ea7a8ac1be26180
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-09-22 17:43:44 +0300

    spiking

--- tools/portfolio-tracker.py
+++ tools/portfolio-tracker.py
@@ -11,19 +11,20 @@ import libs.portfolio
 
 if __name__ == "__main__":
 
-    portfolio = libs.portfolio.PORTFOLIO(name='SBER', money=100000)
-    portfolio.add(symbol='FXIT', share=30)
-    portfolio.add(symbol='SBSP', share=25)
-    portfolio.add(symbol='SBCB', share=15)
-    portfolio.add(symbol='FXTB', share=10)
-    portfolio.add(symbol='FXRU', share=10)
-    portfolio.add(symbol='MOEX', share=10, asset_type='stock')
+    # portfolio = libs.portfolio.PORTFOLIO(name='SBER', money=100000)
+    # portfolio.add(symbol='FXIT', share=30)
+    # portfolio.add(symbol='SBSP', share=20)
+    # portfolio.add(symbol='SBCB', share=10)
+    # portfolio.add(symbol='FXTB', share=10)
+    # portfolio.add(symbol='FXRU', share=10)
+    # portfolio.add(symbol='MOEX', share=20, asset_type='stock')
 
-    # portfolio = libs.portfolio.PORTFOLIO(name='Kubyshka', money=1000000)
-    # portfolio.add(symbol='FXRL', share=25)
-    # portfolio.add(symbol='FXUS', share=25)
-    # portfolio.add(symbol='FXGD', share=25)
-    # portfolio.add(symbol='FXRU', share=25)
+    portfolio = libs.portfolio.PORTFOLIO(name='Kubyshka', money=100000)
+    portfolio.add(symbol='FXRL', share=20)
+    portfolio.add(symbol='FXUS', share=10)
+    portfolio.add(symbol='FXIT', share=30)
+    portfolio.add(symbol='FXGD', share=20)
+    portfolio.add(symbol='FXRU', share=20)
 
     portfolio.run()
 
