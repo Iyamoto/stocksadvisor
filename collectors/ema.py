30b1f6d9b766aa87a10abdccc344156fead73de0
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-10-05 13:26:21 +0300

    spiking

---
+++ collectors/ema.py
@@ -0,0 +1,67 @@
+"""Collect EMA 200"""
+
+
+import sys
+import os
+sys.path.insert(0, os.path.abspath('..'))
+import json
+import time
+import logging
+import fire
+import requests
+from influxdb import InfluxDBClient
+import configs.alphaconf
+import configs.influx
+import configs.fxit
+
+
+def get_ema200_alpha(symbol, key='demo'):
+    url = 'https://www.alphavantage.co/query?function=' + \
+          'EMA&symbol={}&interval=daily&time_period=200&series_type=close&apikey={}'.format(symbol, key)
+    retry = 0
+    ema200 = None
+    while True:
+        try:
+            r = requests.get(url=url)
+            data = r.json()
+            last = data['Meta Data']['3: Last Refreshed']
+            ema200 = float(data['Technical Analysis: EMA'][last]['EMA'])
+            if ema200:
+                break
+        except:
+            retry += 1
+            if retry > 10:
+                logging.error('Can not fetch ' + symbol)
+            time.sleep(retry)
+            continue
+
+    return ema200
+
+
+def fetch_ema200_fxit(write_to_influx=True):
+    if write_to_influx:
+        influx_client = InfluxDBClient(
+            configs.influx.HOST,
+            configs.influx.PORT,
+            configs.influx.DBUSER,
+            configs.influx.DBPWD,
+            configs.influx.DBNAME,
+            timeout=5
+        )
+
+    symbols = configs.fxit.holdings
+    for symbol in symbols:
+        ema200 = get_ema200_alpha(symbol=symbol, key=configs.alphaconf.key)
+        print(ema200)
+
+        if write_to_influx:
+            pass
+
+
+
+if __name__ == "__main__":
+    if "PYCHARM_HOSTED" in os.environ:
+        fetch_ema200_fxit(write_to_influx=False)
+    else:
+        fire.Fire()
+
