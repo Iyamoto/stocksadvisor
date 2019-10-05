4f729f77ce4ffa4789067165cce4bf381c5fa5d8
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-10-05 15:49:16 +0300

    spiking

--- collectors/ema.py
+++ collectors/ema.py
@@ -4,7 +4,7 @@
 import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
-import json
+import datetime
 import time
 import logging
 import fire
@@ -15,7 +15,8 @@ import configs.influx
 import configs.fxit
 
 
-def get_ema200(symbol):
+def get_ema200(symbol, age=23*3600):
+
     influx_client = InfluxDBClient(
         configs.influx.HOST,
         configs.influx.PORT,
@@ -27,7 +28,16 @@ def get_ema200(symbol):
 
     query = 'SELECT last("ema200") FROM "ema200" WHERE ("symbol"=~ /' + symbol + '/)'
     result = influx_client.query(query)
-    ema200 = result.raw['series'][0]['values'][0][1]
+    ema200_date = result.raw['series'][0]['values'][0][0]
+    tmp = ema200_date.split('.')
+    ema200_date = tmp[0]
+    ema200_date = datetime.datetime.strptime(ema200_date, '%Y-%m-%dT%H:%M:%S')
+    now = datetime.datetime.utcnow()
+    diff = now - ema200_date
+    if diff.total_seconds() < age:
+        ema200 = result.raw['series'][0]['values'][0][1]
+    else:
+        ema200 = None
     return ema200
 
 
@@ -35,23 +45,29 @@ def fetch_ema200_alpha(symbol, key=configs.alphaconf.key):
     url = 'https://www.alphavantage.co/query?function=' + \
           'EMA&symbol={}&interval=daily&time_period=200&series_type=close&apikey={}'.format(symbol, key)
     retry = 0
-    ema200 = None
-    while True:
-        try:
-            r = requests.get(url=url)
-            data = r.json()
-            last = data['Meta Data']['3: Last Refreshed']
-            ema200 = float(data['Technical Analysis: EMA'][last]['EMA'])
-            if ema200:
-                break
-        except:
-            retry += 1
-            if retry > 10:
-                logging.error('Can not fetch ' + symbol)
-                logging.error(url)
-                break
-            time.sleep(retry)
-            continue
+
+    # Check cache
+    ema200 = get_ema200(symbol)
+    if ema200:
+        logging.info(symbol + ' Got EMA200 from InfluxDB: ' + str(ema200))
+    else:
+        # Try to fetch from the WEB
+        while True:
+            try:
+                r = requests.get(url=url)
+                data = r.json()
+                last = data['Meta Data']['3: Last Refreshed']
+                ema200 = float(data['Technical Analysis: EMA'][last]['EMA'])
+                if ema200:
+                    break
+            except:
+                retry += 1
+                if retry > 10:
+                    logging.error('Can not fetch ' + symbol)
+                    logging.error(url)
+                    break
+                time.sleep(retry)
+                continue
 
     return ema200
 
@@ -91,11 +107,11 @@ def fetch_ema200_fxit(write_to_influx=True):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s',
+                        level='INFO',
+                        stream=sys.stderr)
     if "PYCHARM_HOSTED" in os.environ:
         fetch_ema200_fxit(write_to_influx=False)
     else:
-        logging.basicConfig(format='%(asctime)-15s %(levelname)s %(message)s',
-                            level='INFO',
-                            stream=sys.stderr)
         fire.Fire()
 
