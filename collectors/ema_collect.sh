427cd0a2e9842b5378be57d95d41dc458a9dca92
iyamoto@gmail.com <5tgb8ik,5tgb>
2019-10-19 11:39:22 +0300

    spiking

--- collectors/ema_collect.sh
+++ collectors/ema_collect.sh
@@ -2,5 +2,9 @@
 
 cd /home/alex/stocksadvisor/
 /usr/bin/env git pull
+
 cd /home/alex/stocksadvisor/collectors
 /usr/bin/env python3 ema.py fetch --datatype portfolio
+
+cd /home/alex/stocksadvisor/tools
+/usr/bin/env python3 vix.py
